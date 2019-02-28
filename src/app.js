832885388469ad28e9af1c9650323f5f639774ff
aindriu80 <aindriu80@gmail.com>
2019-02-28 16:18:57 +0000

    Adding posts to db file with JSON-SERVER

--- src/app.js
+++ src/app.js
@@ -2,10 +2,32 @@
  import { ui } from './ui';
 
  // Get posts on DOM load
- document.addEventListener('DOMContentLoaded', getPosts);
+document.addEventListener('DOMContentLoaded', getPosts);
 
- function getPosts(){
+// Listen for add posts
+document.querySelector('.post-submit').addEventListener('click', submitPosts);
+
+// Get Posts
+function getPosts() {
      http.get('http://localhost:3000/posts')
      .then(data => ui.showPosts(data))
      .catch(err => console.log(err));
- }
+}
+
+
+// Submit Posts
+function submitPosts() {
+    const title = document.querySelector('#title').value;
+    const body = document.querySelector('#title').value;
+
+    const data = {
+        title,
+        body,
+    }
+    // Create Post
+    http.post('http://localhost:3000/posts', data)
+        .then(data => {
+            getPosts();
+        })
+        .catch(err => console.log(err));
+}
